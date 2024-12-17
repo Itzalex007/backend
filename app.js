5395637a21f662b263acb8726b7385ca83c99026
Itzalex007 <91385097+Itzalex007@users.noreply.github.com>
2024-12-17 16:20:26 +0530

    Update app.js

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["https://minhaj-food.netlify.app/"],
     methods: ["POST"],
     credentials: true,
   })
